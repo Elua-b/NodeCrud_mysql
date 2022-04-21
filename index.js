f3f014a7579b8a9fc120c23259ec351aa208f777
gershomlapaix <nsengiyumv33@gmail.com>
2022-04-21 14:16:53 +0200

    Node js and mysql

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const mysql= require ('mysql');
 const express=require('express');
 const app=express();
 const bodyparser=require('body-parser');
-app.use(bodyparser.json);
 
+app.use(bodyparser.json());
+
+const port=7070
 var mysqlConnection=mysql.createConnection({
    host:'localhost',
    user:'root',
@@ -17,20 +19,35 @@ mysqlConnection.connect((err)=>{
     console.log('DB connection failed \n Error :' + JSON.stringify(err,undefined,2));
 });
 
-app.listen(3000,()=>console.log('Express server is running on the port  on:3000'));
+app.listen(port,()=>console.log(`Express server is running on the port  on: ${port}`));
+
 app.get('/employees',(req,res)=>{
-    mysqlConnection.query('SELECT * FROM Employee',(err,rows,fields)=>{
-        if(!err)
-       res.send(rows);
-        else
-        console.log(err);
+    mysqlConnection.query('SELECT * FROM Employee',(err,rows)=>{
+        if(err){
+            console.log(err);
+        }else{
+            res.json({
+                status:200,
+                data:rows
+            })
+        }
     })
+  
 });
 app.delete('/employees/:id',(req,res)=>{
-    mysqlConnection.query('DELETE  FROM Employee WHERE EmpID=?',[req.params.id],(err,rows,fields)=>{
-        if(!err)
-       res.send('Deleted sucessfully');
-        else
-        console.log(err);
-    })
+    try {
+        mysqlConnection.query('DELETE  FROM Employee WHERE EmpID=?',[req.params.id],(err,message)=>{
+            if(err){
+                throw err;
+            }else{
+                res.json({
+                    status:304,
+                    message:"user was deleted!"
+                })
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+   
 });
